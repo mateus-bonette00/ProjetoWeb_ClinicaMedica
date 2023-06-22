84155dcd26ab5c6198615b77ed3efc1a7eedc1b2
Thalles Alexsander <thallesander@hotmail.com>
2023-06-22 20:45:06 -0300

    Editing button schedule

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import "./App.css";
+import ButtonSchedule from "./components/ButtonSchedule";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
       <body className="flex">
-        <section className="flex flex-row flex-wrap">
-          <div>
-            <h1>TRANSFORME SUA SAÚDE!</h1>
-            <h1>TRANSFORME SEU NODA!</h1>
-
-            <button className="w-48 h-12 bg-green-500 border-solid rounded-full">
-              AGENDE AGORA
-            </button>
-          </div>
-          <div>
-            <p className="w-1/3 text-left">
-              Com nossa equipe especializada e técnicas comprovadas,
-              ajudamos você superar todos os seus problemas!
-            </p>
-          </div>
-        </section>
+        <ButtonSchedule/>
       </body>
     </div>
   );
---
+++ src/components/ButtonSchedule.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ButtonSchedule() {
+  return (
+    <div>
+      <button className="bg-greeny w-96 h-20 rounded-full font-bold text-white text-2xl hover:bg-white hover:text-black">
+        {" "}
+        AGENDE AGORA{" "}
+      </button>
+    </div>
+  );
+}
+
+export default ButtonSchedule;
