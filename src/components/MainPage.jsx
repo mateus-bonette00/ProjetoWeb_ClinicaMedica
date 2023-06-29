5035693124bae1043c202e9dad03aac5d0473182
Thalles Alexsander <thallesander@hotmail.com>
2023-06-29 17:10:32 -0300

    editando imagem de fundo na seção principal

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import imagemHome from "./img/imageHome.png";
 
 function MainPage() {
   return (
-    <section className="bg-slate-600 flex flex-row w-screen h-96 justify-center">
+    <section className="bg-imagemHome flex flex-row w-screen h-96 justify-center">
       <div className=" mt-40 ml-36">
-        <h1 className="text-white font-bold text-3xl">TRANSFORME <br></br>SUA SAÚDE!</h1>
+        <h1 className="text-white font-bold text-3xl">
+          TRANSFORME <br></br>SUA SAÚDE!
+        </h1>
         <button className="bg-greeny w-60 h-12 rounded-full mt-4 hover:bg-black text-white">
           AGENDE AGORA
         </button>
       </div>
       <div className="">
         <h2 className="text-white mt-40 ml-44 text-xl ">
-          Com nossa equipe especializada e técnicas comprovadas,<br></br>
+          Com nossa equipe especializada e técnicas comprovadas,
+          <br></br>
           ajudamos você superar todos os seus problemas!
         </h2>
       </div>
