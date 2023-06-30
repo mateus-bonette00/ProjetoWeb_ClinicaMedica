24db5234194ac8cec6a803c2464c269f1017e39e
Thalles Alexsander <thallesander@hotmail.com>
2023-06-29 21:54:22 -0300

    editando seção principal

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -3,19 +3,18 @@ import imagemHome from "./img/imageHome.png";
 
 function MainPage() {
   return (
-    <section className="bg-imagemHome flex flex-row w-screen h-96 justify-center">
-      <div className=" mt-40 ml-36">
-        <h1 className="text-white font-bold text-3xl">
-          TRANSFORME <br></br>SUA SAÚDE!
+    <section className="bg-imagemHome flex flex-row w-screen h-screen mt-[-20px] justify-center">
+      <div className="mt-40">
+        <h1 className="text-white font-bold text-6xl w-[531px]">
+          TRANSFORME SUA SAÚDE!
         </h1>
-        <button className="bg-greeny w-60 h-12 rounded-full mt-4 hover:bg-black text-white">
+        <button className="bg-greeny w-[300px] h-[70px] rounded-full mt-4 hover:bg-black text-white text-2xl">
           AGENDE AGORA
         </button>
       </div>
-      <div className="">
-        <h2 className="text-white mt-40 ml-44 text-xl ">
+      <div>
+        <h2 className="text-white w-[670px] text-4xl mt-40 ml-20">
           Com nossa equipe especializada e técnicas comprovadas,
-          <br></br>
           ajudamos você superar todos os seus problemas!
         </h2>
       </div>
