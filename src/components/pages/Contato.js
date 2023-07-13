3613e505ea743e583e8dd9d7aaa12a2bd0f826fd
Gksnoda <gksnoda@hotmail.com>
2023-07-13 20:19:07 -0300

    Arrumei o problema com o css

--- src/components/pages/Contato.js
+++ src/components/pages/Contato.js
@@ -4,18 +4,12 @@ import WppBotao from '../WppBotao';
 
 function Contato (){
     return (
-        <div className="PageContato mb-12">
+        <div className="PageContato">
           <MainPageContato />
-<<<<<<< HEAD
           <div className="tema">
-            <h1 className="text-greeny justify-center text-7xl mb-5 mt-20">Contato</h1>
+            <h1 className="text-greeny justify-center text-7xl mb-12 mt-20">Contato</h1>
             <h2 className="textoContato">Estamos sempre disponíveis para responder quaisquer dúvidas ou sugestões!</h2>
-
           </div>
-=======
-          <h1 className="mt-10">Contato</h1>
-          <h2>Estamos sempre disponíveis para responder quaisquer dúvidas ou sugestões!</h2>
->>>>>>> 52a006f53752fdcb7350af0c1bc1d733e15a0b2b
           <ContatoTema />
           <WppBotao />
         
